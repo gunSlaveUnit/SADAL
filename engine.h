81d2a43f5e75b79d4fe8264ca47798da15585a36
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-02-03 21:31:52 +0300

    Added statement to enable validation layers and required validation layers.

--- engine.h
+++ engine.h
@@ -9,6 +9,7 @@
 
 #include <iostream>
 #include <cstdint>
+#include <vector>
 
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
@@ -30,6 +31,15 @@ private:
     GLFWwindow* window;
 
     VkInstance instance;
+
+#ifdef NDEBUG
+    const bool enableValidationLayers = false;
+#else
+    const bool enableValidationLayers = true;
+#endif
+    const std::vector<const char*> validationLayers = {
+            "VK_LAYER_KHRONOS_validation"
+    };
 };
 
 
