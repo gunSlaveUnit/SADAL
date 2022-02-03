97021d1c91be3be39fc4b009ad69aa7e585410d8
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-02-03 16:27:06 +0300

    Instance is created

--- engine.cpp
+++ engine.cpp
@@ -46,6 +46,9 @@ void Engine::createInstance() {
     createInfo.ppEnabledExtensionNames = glfwExtensionsNames;
 
     createInfo.enabledLayerCount = 0;
+
+    if(vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+        throw std::runtime_error("ERROR: Vulkan can't create instance");
 }
 
 void Engine::mainLoop() {
