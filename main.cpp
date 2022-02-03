c45c615da452749d2efbbc1b270ff920f02d8ab0
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-02-03 15:52:04 +0300

    Created engine object in main

--- main.cpp
+++ main.cpp
@@ -1,2 +1,15 @@
+#include <iostream>
+#include <stdexcept>
+
+#include "engine.h"
+
 int main() {
+    try {
+        Engine engine;
+        engine.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() <<std::endl;
+        return EXIT_FAILURE;
+    }
+    return EXIT_SUCCESS;
 }
