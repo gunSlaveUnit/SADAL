1bd32d435038d1784eb512ab180dbd80491b2234
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-02-03 21:54:13 +0300

    In validation layers changed name of one validation layer. Now it's a standard lunarg validation

--- engine.h
+++ engine.h
@@ -40,7 +40,7 @@ private:
     const bool enableValidationLayers = true;
 #endif
     const std::vector<const char*> validationLayers = {
-            "VK_LAYER_KHRONOS_validation"
+            "VK_LAYER_LUNARG_standard_validation"
     };
 };
 
