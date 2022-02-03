981b90d50f2862a34832d661bae7002a529f9284
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-02-03 16:25:24 +0300

    Added create info structure

--- engine.cpp
+++ engine.cpp
@@ -27,13 +27,25 @@ void Engine::initVulkan() {
 }
 
 void Engine::createInstance() {
-    VkApplicationInfo appInfo;
+    VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     appInfo.pEngineName = "No engine";
     appInfo.engineVersion = VK_MAKE_VERSION(0, 0, 1);
     appInfo.pApplicationName = WINDOW_TITLE;
     appInfo.applicationVersion = VK_MAKE_VERSION(0, 0, 1);
     appInfo.apiVersion = VK_API_VERSION_1_2;
+
+    VkInstanceCreateInfo createInfo{};
+    createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    createInfo.pApplicationInfo = &appInfo;
+
+    uint32_t glfwExtensionCount = 0;
+    const char** glfwExtensionsNames = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+    createInfo.enabledExtensionCount = glfwExtensionCount;
+    createInfo.ppEnabledExtensionNames = glfwExtensionsNames;
+
+    createInfo.enabledLayerCount = 0;
 }
 
 void Engine::mainLoop() {
