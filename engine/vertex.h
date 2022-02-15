bc1ecfc418e632f2cfe4abf384fdfb7cc5c86978
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-02-15 12:32:59 +0300

    Added a static method to get vertex attribute description

--- engine/vertex.h
+++ engine/vertex.h
@@ -7,6 +7,8 @@
 #ifndef SHATTEREDSOULS_VERTEX_H
 #define SHATTEREDSOULS_VERTEX_H
 
+#include <array>
+
 #define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
@@ -20,6 +22,22 @@ struct Vertex {
         bindingDescription.inputRate = VK_VERTEX_INPUT_RATE_VERTEX;
 
         return bindingDescription;
+    }
+
+    static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions() {
+        std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions{};
+
+        attributeDescriptions[0].binding = 0;
+        attributeDescriptions[0].location = 0;
+        attributeDescriptions[0].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attributeDescriptions[0].offset = offsetof(Vertex, position);
+
+        attributeDescriptions[1].binding = 0;
+        attributeDescriptions[1].location = 1;
+        attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attributeDescriptions[1].offset = offsetof(Vertex, color);
+
+        return attributeDescriptions;
     }
 
     glm::vec3 position;
