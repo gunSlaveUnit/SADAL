8bfaf81b19a793a3507ead323bcdcd15b3e3953c
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-02-15 11:34:16 +0300

    Created vertex struct

---
+++ engine/vertex.cpp
@@ -0,0 +1,5 @@
+//
+// Created by gunSlaveUnit on 2/15/22.
+//
+
+#include "vertex.h"
---
+++ engine/vertex.h
@@ -0,0 +1,16 @@
+//
+// Created by gunSlaveUnit on 2/15/22.
+//
+
+#pragma once
+
+#ifndef SHATTEREDSOULS_VERTEX_H
+#define SHATTEREDSOULS_VERTEX_H
+
+
+struct Vertex {
+
+};
+
+
+#endif //SHATTEREDSOULS_VERTEX_H
