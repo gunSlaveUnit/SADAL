a5f4f8d815383f23b28d21d2ae6fb0623d8ab0d2
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-02-18 19:25:13 +0300

    Added a using of layout descriptor

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -513,8 +513,8 @@ void Engine::createGraphicsPipeline() {
 
     VkPipelineLayoutCreateInfo pipelineLayoutInfo{};
     pipelineLayoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
-    pipelineLayoutInfo.setLayoutCount = 0;
-    pipelineLayoutInfo.pSetLayouts = nullptr;
+    pipelineLayoutInfo.setLayoutCount = 1;
+    pipelineLayoutInfo.pSetLayouts = &descriptorSetLayout;
     pipelineLayoutInfo.pushConstantRangeCount = 0;
     pipelineLayoutInfo.pPushConstantRanges = nullptr;
 
@@ -913,6 +913,7 @@ void Engine::recreateSwapChain() {
 
 void Engine::cleanup() {
     cleanupSwapChain();
+    vkDestroyDescriptorSetLayout(logicalDevice, descriptorSetLayout, nullptr);
     vkDestroyBuffer(logicalDevice, indexBuffer, nullptr);
     vkFreeMemory(logicalDevice, indexBufferMemory, nullptr);
     vkDestroyBuffer(logicalDevice, vertexBuffer, nullptr);
