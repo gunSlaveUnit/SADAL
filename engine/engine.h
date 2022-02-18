fd75c8f64f9110cde9612f0b739adfc44946e78e
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-02-18 18:25:20 +0300

    Changed vertices color

--- engine/engine.h
+++ engine/engine.h
@@ -125,10 +125,10 @@ private:
     VkDeviceMemory indexBufferMemory;
 
     const std::vector<Vertex> vertices = {
-            {{-0.5f, -0.5f, 0.0f}, {1.0f, 0.0f, 0.0f}},
-            {{0.5f, -0.5f, 0.0f}, {0.0f, 1.0f, 0.0f}},
-            {{0.5f, 0.5f, 0.0f}, {0.0f, 0.0f, 1.0f}},
-            {{-0.5f, 0.5f, 0.0f}, {1.0f, 1.0f, 1.0f}}
+            {{-0.5f, -0.5f, 0.0f}, {0.5f, 0.0f, 0.0f}},
+            {{0.5f, -0.5f, 0.0f}, {0.5f, 0.0f, 0.0f}},
+            {{0.5f, 0.5f, 0.0f}, {0.0f, 0.0f, 0.5f}},
+            {{-0.5f, 0.5f, 0.0f}, {0.0f, 0.0f, 0.5f}}
     };
     const std::vector<uint32_t> indices = {
             0, 1, 2, 2, 3, 0
