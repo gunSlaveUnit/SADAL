2ef6d7b46483d28be8ba6cb97711b0217fdd5675
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-02-19 13:18:13 +0300

    Added stb_image.h lib

--- engine/engine.h
+++ engine/engine.h
@@ -21,6 +21,8 @@
 #define GLM_FORCE_RADIANS
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 #include "vertex.h"
 #include "transformation.h"
