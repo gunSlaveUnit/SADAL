2aab7c8fca286e431fc4ffab1754e482063200e1
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-02-20 11:26:05 +0300

    Added the correct access masks and pipeline stages

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -774,6 +774,12 @@ void Engine::transitionImageLayout(VkImage image, VkFormat format, VkImageLayout
 
         sourceStage = VK_PIPELINE_STAGE_TRANSFER_BIT;
         destinationStage = VK_PIPELINE_STAGE_FRAGMENT_SHADER_BIT;
+    } else if (oldLayout == VK_IMAGE_LAYOUT_UNDEFINED && newLayout == VK_IMAGE_LAYOUT_DEPTH_STENCIL_ATTACHMENT_OPTIMAL) {
+        barrier.srcAccessMask = 0;
+        barrier.dstAccessMask = VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_READ_BIT | VK_ACCESS_DEPTH_STENCIL_ATTACHMENT_WRITE_BIT;
+
+        sourceStage = VK_PIPELINE_STAGE_TOP_OF_PIPE_BIT;
+        destinationStage = VK_PIPELINE_STAGE_EARLY_FRAGMENT_TESTS_BIT;
     } else
         throw std::invalid_argument("ERROR: unsupported layout transition");
 
