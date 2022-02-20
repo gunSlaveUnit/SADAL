092d88685fd7cf370f1b860efcaf70d62c72d749
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-02-20 11:59:57 +0300

    Specified multiple clear values

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -1126,9 +1126,14 @@ void Engine::createCommandBuffers() {
         renderPassInfo.framebuffer = swapChainFrameBuffers[i];
         renderPassInfo.renderArea.offset = {0, 0};
         renderPassInfo.renderArea.extent = swapChainExtent;
+
+        std::array<VkClearValue, 2> clearValues{};
+        clearValues[0].color = {{0.0f, 0.0f, 0.0f, 1.0f}};
+        clearValues[1].depthStencil = {1.0f, 0};
+
         VkClearValue clearColor = {{{0.0f, 0.0f, 0.0f, 1.0f}}};
-        renderPassInfo.clearValueCount = 1;
-        renderPassInfo.pClearValues = &clearColor;
+        renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());;
+        renderPassInfo.pClearValues = clearValues.data();
 
         vkCmdBeginRenderPass(commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
 
