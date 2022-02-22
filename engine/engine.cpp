fb422413015938078b9398278288bbe6a67e3183
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-02-22 18:59:19 +0300

    Pipeline uses more than one sample now

--- engine/engine.cpp
+++ engine/engine.cpp
@@ -530,7 +530,7 @@ void Engine::createGraphicsPipeline() {
     VkPipelineMultisampleStateCreateInfo multisampling{};
     multisampling.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
     multisampling.sampleShadingEnable = VK_FALSE;
-    multisampling.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
+    multisampling.rasterizationSamples = MSAASamples;
     multisampling.minSampleShading = 1.0f;
     multisampling.pSampleMask = nullptr;
     multisampling.alphaToCoverageEnable = VK_FALSE;
