d6f1b6e16b981f3de57476c1cfff87346dae9e97
gunSlaveUnit <rp34789t5409yvt@protonmail.com>
2022-02-22 15:27:24 +0300

    Added generateMipmaps method. All successfully work

--- engine/engine.h
+++ engine/engine.h
@@ -76,6 +76,7 @@ private:
     VkFormat findSupportedFormat(const std::vector<VkFormat>& candidates, VkImageTiling tiling, VkFormatFeatureFlags features);
     void createFrameBuffers();
     void createTexture();
+    void generateMipmaps(VkImage image,  VkFormat imageFormat, int32_t texWidth, int32_t texHeight, uint32_t mipLevels);
     void createImage(uint32_t width, uint32_t height, uint32_t mipLevels, VkFormat format, VkImageTiling tiling, VkImageUsageFlags usage,
                      VkMemoryPropertyFlags properties, VkImage& image, VkDeviceMemory& imageMemory);
     void transitionImageLayout(VkImage image, VkFormat format, VkImageLayout oldLayout, VkImageLayout newLayout, uint32_t mipLevels);
